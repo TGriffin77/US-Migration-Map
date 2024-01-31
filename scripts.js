39f93e126da9e698dd5cd68e2c5fd4d7837bafa0
TJ-Griffin <tjgriffin77@gmail.com>
2024-01-30 21:38:33 -0500

    state click detection

--- scripts.js
+++ scripts.js
@@ -1,19 +1,33 @@
+// Box appearing displaying the name of the state that mouse is hovering over
 var detailsBox = document.getElementById('details-box');
 
 document.addEventListener('mouseover', function (e) {
   if (e.target.tagName == 'path') {
     var content = e.target.dataset.name;
     detailsBox.innerHTML = content;
-    detailsBox.style.opacity = "100%";
+    detailsBox.style.opacity = "70%";
   }
   else {
     detailsBox.style.opacity = "0%";
   }
 });
 
+// Assign a click listener for each state
+let states = document.getElementsByTagName('path')
+let stateClicked = new Array(states.length).fill(false)
+
+for (let i = 0; i < states.length; i++){
+    states[i].addEventListener("click", function(){
+        stateClicked.fill(false)
+        stateClicked[i] = true;
+    });
+}
+
+// Follow the mouse and assign the name of the state box below it
 window.onmousemove = function (e) {
   var x = e.clientX,
       y = e.clientY;
   detailsBox.style.top = (y + 20) + 'px';
-  detailsBox.style.left = (x) + 'px';
+  detailsBox.style.left = (x + 50) + 'px';
 };
+
